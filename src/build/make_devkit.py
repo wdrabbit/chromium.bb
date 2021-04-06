bf7f8531a53962a556b4e00b336885839d4e5e3d
Imran Haider <ihaider3@bloomberg.net>
2021-04-06 11:00:30 -0400

    Remove snapshot_blob from make_devkit.py (#1075)

--- src/build/make_devkit.py
+++ src/build/make_devkit.py
@@ -149,7 +149,6 @@ def copyBin(destDir, version, configs, includeMap):
     'blpwtk2_subprocess' + productAppend + '.exe',
     'content_shell.exe',
     'd8.exe',
-    'snapshot_blob' + productAppend + '.bin',
     'v8_context_snapshot' + productAppend + '.bin',
     'blpwtk2' + productAppend + '.dll',
     'blpcr_egl' + productAppend + '.dll',
